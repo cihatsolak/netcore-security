ffd90f0f7d60c64971f4dfa4a48775c3de3e4996
Cihat Solak <ExtCihatS@d-teknoloji.com.tr>
2021-03-29 01:19:57 +0300

    IPControl.Web project created

---
+++ NetCoreSecurity/IPControl.Web/Middlewares/IPSafeMiddleware.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPControl.Web.Middlewares
+{
+    public class IPSafeMiddleware
+    {
+    }
+}
