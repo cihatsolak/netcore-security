a1450a6c686ca3cd65b5557a2c5dc9e06a3a6c7c
Cihat Solak <ExtCihatS@d-teknoloji.com.tr>
2021-03-30 22:27:07 +0300

    Environment Variables

--- NetCoreSecurity/DataProtection.Web/Program.cs
+++ NetCoreSecurity/DataProtection.Web/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DataProtection.Web
 {
@@ -18,9 +14,18 @@ namespace DataProtection.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(ConfigConfiguration)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void ConfigConfiguration(IConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder
+               .AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true) //appsettings.json
+               .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables(); //Proje özelliklerinde debug kýsýmda tanýmlanýr.
+        }
     }
 }
