efb061f1d954afceca6afc7f2a2b8f0b5ba5d2c1
Cihat Solak <ExtCihatS@d-teknoloji.com.tr>
2021-04-02 13:52:22 +0300

    reflected xss attacks

---
+++ NetCoreSecurity/DataProtection.Web/Controllers/CrossSiteAttacksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Security.Web.Models.ViewModels;
+
+namespace Security.Web.Controllers
+{
+    public class CrossSiteAttacksController : Controller
+    {
+        [HttpGet]
+        public IActionResult VehicleAdd()
+        {
+            //Örnek Cookie Bilgisi Oluşturuyorum.
+            HttpContext.Response.Cookies.Append("username", "cihatsolak");
+            HttpContext.Response.Cookies.Append("password", "123456");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VehicleAdd(VehicleAddViewModel vehicleAddViewModel)
+        {
+            ViewBag.Year = vehicleAddViewModel.Year;
+            ViewBag.Color = vehicleAddViewModel.Color;
+            ViewBag.Plate = vehicleAddViewModel.Plate;
+
+            return View();
+        }
+    }
+}
---
+++ NetCoreSecurity/DataProtection.Web/Models/ViewModels/VehicleAddViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Security.Web.Models.ViewModels
+{
+    public class VehicleAddViewModel
+    {
+        public int Year { get; set; }
+        public string Plate { get; set; }
+        public string Color { get; set; }
+    }
+}
