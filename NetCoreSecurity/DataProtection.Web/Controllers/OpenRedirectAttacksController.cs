9a31eee0f6544d19aaad35b97f020edbcdd26ed7
Cihat Solak <ExtCihatS@d-teknoloji.com.tr>
2021-04-03 17:17:56 +0300

    open redirect attacks

---
+++ NetCoreSecurity/DataProtection.Web/Controllers/OpenRedirectAttacksController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Security.Web.Models.ViewModels;
+
+namespace Security.Web.Controllers
+{
+    public class OpenRedirectAttacksController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            TempData["ReturnUrl"] = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(loginViewModel);
+
+            //kullanıcı adı ve şifreyi burada kontrol ettiğimi varsayıyorum.
+
+            string redirectUrl = TempData["ReturnUrl"] as string;
+
+            //Open redirect attacks'a engel.
+            if (!Url.IsLocalUrl(redirectUrl)) //Bu metot gelen url'in benim domainime ait olup olmadığını tespit etmekte.
+                return Redirect("/");
+
+            return Redirect(redirectUrl);
+        }
+    }
+}
